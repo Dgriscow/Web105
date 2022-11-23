7a192cc1f6f92af5d480dd03d9f3cc2a0d8eecc1
Dgriscow <98046458+Dgriscow@users.noreply.github.com>
2022-11-23 13:19:54 -0600

    Add files via upload

---
+++ image Based Sites/javascript.js
@@ -0,0 +1,9 @@
+
+
+function change(my_image){
+    document.getElementById("large").src = "images/trees.png"
+    document.getElementById("large").src = "images/images/coollake.png"
+    document.getElementById("large").src = "images/images/morewater.png"
+    document.getElementById("large").src = "images/images/Starved_rock.jpeg"
+    document.getElementById("large").src = "images/images/waterfront.jpeg"
+}
